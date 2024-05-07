44037cdf42e6b388ee1cc287c1a30354f965abc8
Tirmary Reyes <tmrv1991@gmail.com>
2024-05-07 15:22:22 +0300

    Add auth, login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect} from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
  
 const FormSchema = z.object({
   id: z.string(),
@@ -44,11 +47,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
     };
   }
 
-  
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
   
   try {
     await sql`
@@ -62,7 +63,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
     };
   }
 
- 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -76,7 +76,6 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
-
 
 export async function updateInvoice(
   id: string,
@@ -108,7 +107,26 @@ export async function updateInvoice(
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
- 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
+  
